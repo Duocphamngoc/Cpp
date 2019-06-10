bd46036638b990e1b3a777b9df33de660a4db998
duocphamngoc <phamngocduoc0880@gmail.com>
2019-06-10 16:42:43 +0700

    done 2 functions Input and Display

---
+++ QUANLY/QUANLY/QUANLY.cpp
@@ -0,0 +1,73 @@
+// QUANLY.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+using namespace std;
+
+struct STUDENT
+{
+	int ID;
+	string NAME;
+	float SCORE;
+};
+STUDENT * student = new STUDENT[3];
+
+
+void input(STUDENT * std) {
+	
+	for (int i = 0; i < 3;i++) {
+		std[i].ID = i + 1;
+		cout << "Enter Name: "<<endl;
+		getline(cin, std[i].NAME);
+		
+		cout << "Enter Score: " << endl;
+		cin >> student[i].SCORE;
+		cin.ignore();
+	}
+
+}
+
+void display() {
+	cout << "ID           FULLNAME          SCORE" << endl;
+	for (int i = 0; i < 3;i++) {
+		cout << student[i].ID << "            " << student[i].NAME << "            " << student[i].SCORE<<endl;
+	}
+
+}
+
+void menu() {
+	cout << "------------------MENU----------------" << endl;
+	cout << "	1. INPUT" << endl;
+	cout << "	2. DISPLAY" << endl;
+	cout << "	3. SAVE TO FILE" << endl;
+	cout << "	4. LOAD FROM FILE" << endl;
+	cout << "	0. EXIT" << endl;
+	cout << "------------------------------------" << endl;
+	cout << "choose: ";
+	int choose;
+	do {
+		cin >> choose;
+		cin.ignore();
+	} while (choose<0 && choose >= 5);
+	switch (choose)
+	{
+	case 0:
+		return;
+	case 1:
+		input(student);
+	case 2:
+		display();
+	}
+
+}
+int main()
+{
+	while (true) {
+		menu();
+	}
+	system("pause");
+    return 0;
+}
+
