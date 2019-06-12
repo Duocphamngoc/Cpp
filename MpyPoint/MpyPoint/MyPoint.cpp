a936e3cb2e3d5a0856b91fd8b8ddbb91dfde8bad
duocphamngoc <phamngocduoc0880@gmail.com>
2019-06-12 16:32:15 +0700

    + Add Contructor Function MyPoint

---
+++ MpyPoint/MpyPoint/MyPoint.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "iostream"
+#include "MyPoint.h"
+#include "string.h"
+
+MyPoint::MyPoint() {};
+MyPoint::MyPoint(int x, int y)
+{
+	mPosX = x;
+	mPosY = y;
+}
+
+int main() {
+	return 0 ;
+}
---
+++ MpyPoint/MpyPoint/MyPoint.h
@@ -0,0 +1,10 @@
+#pragma once
+class MyPoint {
+private:
+	int mPosX;
+	int mPosY;
+public:
+	MyPoint();
+	MyPoint(int, int);
+	
+};
