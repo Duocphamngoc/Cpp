2e0254060432c8136b39a2c3871c76310cd7bb0f
duocphamngoc <phamngocduoc0880@gmail.com>
2019-06-13 13:52:45 +0700

    +ADD Project MyPoint

---
+++ MpyPoint/MpyPoint/MyPoint.h
@@ -0,0 +1,15 @@
+#pragma once
+class MyPoint {
+private:
+	int mPosX;
+	int mPosY;
+public:
+	MyPoint();
+	MyPoint(int, int);
+	void Display();
+	void SetX(int);
+	int GetX();
+	void SetY(int);
+	int GetY();
+	float Distance(MyPoint* p);
+};
